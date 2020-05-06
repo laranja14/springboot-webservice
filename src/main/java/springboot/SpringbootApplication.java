aedaafa362a80520a6afbd6d03b20a3aedf3a226
NoJin <laranja1419@gmail.com>
2020-05-06 15:02:13 +0900

    Test 코드 수정

--- src/main/java/springboot/SpringbootApplication.java
+++ src/main/java/springboot/SpringbootApplication.java
@@ -2,15 +2,12 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+// @EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(...)
-		// ���� WAS ����
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
